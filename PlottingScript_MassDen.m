fbbc45bc02a27cfff67141a77e0bd90f016028bc
brodkemd <marekbrodke@gmail.com>
2021-12-05 21:15:46 -0500

    fixing some problems

--- PlottingScript_MassDen.m
+++ PlottingScript_MassDen.m
@@ -7,11 +7,9 @@ n = 16;
 
 % open files for reading
 
-fileID_D = fopen( ...
-'D:\OneDrive - University of Cincinnati 1\research\software\quantum_NS_solver\Gaitan_quantum_Navier_Stokes_simulation_software\MrhoDvals', 'r');
+fileID_D = fopen( 'MrhoDvals', 'r');
 
-fileID_E = fopen( ...
-'D:\OneDrive - University of Cincinnati 1\research\software\quantum_NS_solver\Gaitan_quantum_Navier_Stokes_simulation_software\MrhoEvals', 'r');
+fileID_E = fopen( 'MrhoEvals', 'r');
 
 % size arrays to store data
 

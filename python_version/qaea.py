32d3bbdb8353e9fa5de24d7a28b339b7c32c0a0e
brodkemd <marekbrodke@gmail.com>
2022-01-28 23:48:22 -0500

    Adding circuit visualization functionality

--- python_version/qaea.py
+++ python_version/qaea.py
@@ -1,15 +1,15 @@
 p = 0.2
 num_qbits = 5
 
-if 1:
+if 0:
     from qiskit.algorithms import EstimationProblem, AmplitudeEstimation
+    
     from qiskit.circuit import QuantumCircuit  
     from qiskit.utils import QuantumInstance
     from qiskit import BasicAer, transpile
     
     import matplotlib.pyplot as plt
     import numpy as np
-
 
 
     class BernoulliA(QuantumCircuit):
@@ -71,9 +71,17 @@ if 1:
     plt.show()
 
 else:
-    from qiskit import QuantumCircuit
+    from qiskit.quantum_info import Statevector
+    from qiskit import QuantumCircuit, Aer
     import matplotlib.pyplot as plt
     import numpy as np
+
+    def simulated_state_vector(quantum_circuit):
+        backend = Aer.get_backend('statevector_simulator')
+        job = backend.run(quantum_circuit)
+        result = job.result()
+
+        return result
 
     class qaea(QuantumCircuit):
         '''
@@ -84,8 +92,13 @@ else:
         possibly make some upgrades in the future
 
         '''
+        def add(self, string):
+            self.strings.append(string.replace("self", "qc"))
+
         def __init__(self, p, num_qbits):
             self.num_qbits = num_qbits
+
+            self.strings = [f"qc = QuantumCircuit({num_qbits})"]
 
             # creates the circuit
             super().__init__(self.num_qbits)
@@ -93,10 +106,14 @@ else:
             # inits the qubits
             for i in range(1, self.num_qbits):
                 self.h(i)
+                self.add(f"self.h({i})")
                 self.p(0, i)
+                self.add(f"self.p(0, {i})")
+                
 
             theta_p = 2 * np.arcsin(np.sqrt(p))
             self.ry(theta_p, 0)
+            self.add(f"self.ry({theta_p}, 0)")
 
             # for debugging
             print("theta_p:", theta_p)
@@ -105,6 +122,8 @@ else:
             # running the function that generates the main portion of the algorithm
             self.loop()
 
+            self.strings.append("print(qc.draw())")
+
 
         def repeat_gates(self):
             '''
@@ -112,8 +131,13 @@ else:
 
             '''
             self.sdg(0)
+            self.add("self.sdg(0)")
+            
             self.h(0)
+            self.add("self.h(0)")
+
             self.sdg(0)
+            self.add("self.sdg(0)")
         
         
         def end_gates(self):
@@ -123,11 +147,15 @@ else:
             '''
             for i in range(1, self.num_qbits - 1):
                 self.h(i)
+                self.add(f"self.h({i})")
                 for j in range(i + 1, self.num_qbits):
                     self.cp(-1 * np.pi / (2**(j - i)), i, j)
+                    self.add(f"self.cp({-1 * np.pi / (2**(j - i))}, {i}, {j})")
+                
                 self.barrier()
             
             self.h(self.num_qbits - 1)
+            self.add(f"self.h({self.num_qbits - 1})")
 
 
         def loop(self):
@@ -136,38 +164,72 @@ else:
             
             '''
             count = self.num_qbits - 1
-            val = 0
             b = False
+
+            vals = [2.21429743558818, 4.06888787159, 1.28700221758657, 4.99618308959302, -0.567588218416656, 6.85077352559624, -4.27676909042310, 10.5599543976027]
+
+            itr = 0
             while True:
                 self.cx(int(np.ceil(count)), 0)
+                self.add(f"self.cx({int(np.ceil(count))}, 0)")
 
-                if (b): self.p(0, 0)
+                if (b):
+                    self.p(0, 0)
+                    self.add(f"self.p(0, 0)")
 
                 self.repeat_gates()
-                self.p(val, 0)
+                self.p(vals[itr], 0)
+                self.add(f"self.p({vals[itr]}, 0)")
                 self.repeat_gates()
                 self.p(3 * np.pi, 0)
+                self.add(f"self.p({3 * np.pi}, 0)")
                 
                 self.barrier() # for looks
                 if count < 1: break
                 count-=0.5
                 b = not(b)
+                itr+=1
             
             # adds the gates to the end
             self.end_gates()
 
+
+        def run_string(self):
+            for item in self.strings:
+                print(f"Running: {item}")
+                exec(item)
+
+        def show_state_vector_evolution(self):
+            import sys
+            original_stdout = sys.stdout
+            with open('out.txt', 'w') as f:
+                sys.stdout = f # Change the standard output to the file we created.
+                #print('This message will be written to a file.')
+                #sys.stdout = original_stdout 
+                for item in self.strings:
+                    print(item)
+                    exec(item)
+                    exec("result = simulated_state_vector(qc)")
+                    exec("print(result.get_statevector(qc, decimals=3))")
+                    input()
+
+            sys.stdout = original_stdout
 
         def disp(self):
             '''
             draws the circuit in a matplotlib window then displays it
 
             '''
-            self.draw(output='mpl')
-            plt.show()
+            for item in self.strings:
+                print(item)
+            #self.draw(output='mpl')
+            #plt.show()
 
 
     # running the above class
     inst = qaea(p, num_qbits)
 
     # showing the circuit it generates
-    inst.disp()
+    #inst.disp()
+    #inst.run_string()
+    inst.show_state_vector_evolution()
